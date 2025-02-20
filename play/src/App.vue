3a24903d01f61ad15a1d72bf297c6d32e2fb6682
“minorcell” <minorcell6789@gmail.com>
2025-02-20 20:48:45 +0800

    Fix countup

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,19 +1,25 @@
-<script setup lang="ts">
-import { vCountup } from "@cp-vuedir/core";
-</script>
-
 <template>
-  <div class="container">
-    <span v-countup>1222.99999</span>
-  </div>
+  <div class="drag-container">dawdadawdawda</div>
 </template>
 
+<script setup></script>
+
 <style scoped>
-.container {
-  height: 100vh;
-  width: 100vw;
+.drag-container {
+  width: 500px;
+  height: 300px;
+  border: 2px solid #333;
+  position: relative;
+}
+
+.drag-item {
+  width: 100px;
+  height: 100px;
+  background-color: #4caf50;
+  color: white;
   display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
+  cursor: pointer;
 }
 </style>
